aa4a2372cae7d89193d9defc29186898c65d9af7
Naidan <naidan.salvador2@hotmail.com>
2024-08-21 21:31:07 +0100

    Make add to cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -47,12 +47,42 @@ products.forEach((product)=>{
             Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-to-cart"
+            data-product-id="${product.id}">
             Add to Cart
             </button>
         </div>
     `;
 })
 
-console.log(productsHTML);
+// the purpose a data attribute is to store data in the DOM in a way that is accessible to JavaScript
+// we can attach the products name to the button and when we click on it we can access the name of the product
+// when using data attributes in HTML, the convention is to use lowercase and hyphens (Kebab)
+
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click',()=>{
+        const productId = button.dataset.productId; // it goes from kebab case to camel case (Check the console without the .productName)
+        
+        let matchingItem;
+
+
+        cart.forEach((item)=>{
+            if(productId === item.productId){
+                matchingItem = item;
+            }
+        });
+        if(matchingItem){
+            matchingItem.quantity += 1;
+        }
+        else{
+            cart.push({
+                productId: productId,
+                quantity : 1
+            });
+        }
+        
+        console.log(cart);
+    }); 
+});
