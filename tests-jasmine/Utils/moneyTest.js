0a5bab3e0a77fe3f9c5d8c18d9da852de83e02b2
Naidan <naidan.salvador2@hotmail.com>
2024-08-30 22:52:58 +0100

    Create automated tests for addToCart

--- tests-jasmine/Utils/moneyTest.js
+++ tests-jasmine/Utils/moneyTest.js
@@ -1,4 +1,4 @@
-import{formatCurrency} from '../scripts/utils/money.js';
+import {formatCurrency} from '../../scripts/utils/money.js';
 
 describe('test suite : formatCurrency', () => {
     it('Converts Cents into dollars', ()=>{
@@ -7,7 +7,7 @@ describe('test suite : formatCurrency', () => {
     it('works with 0', ()=>{
         expect(formatCurrency(0)).toEqual('0.00')
     });
-    it('rounds up to the neares cent',() =>{
+    it('rounds up to the nearest cent',() =>{
         expect(formatCurrency(2000.5)).toEqual('20.01');
     });
 });
